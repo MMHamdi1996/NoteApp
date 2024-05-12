db87f156d54c432f02246c4fe74aef291cc504e7
mohammadmahdi <hamdimohammadmahdi@gmail.com>
2024-05-12 21:30:33 +0330

    On the way

--- app/src/main/java/com/example/noteapp/LoginActivity.kt
+++ app/src/main/java/com/example/noteapp/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.noteapp
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,10 +11,16 @@ import com.example.noteapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var preferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityLoginBinding.inflate(LayoutInflater.from(this))
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        preferences = androidx.preference.PreferenceManager.getDefaultSharedPreferences(this)
+        editor = preferences.edit()
+        editor.apply()
 
         binding.loginButton.setOnClickListener {
 
@@ -20,11 +28,36 @@ class LoginActivity : AppCompatActivity() {
             val passEditText = binding.passwordEditText.text.toString()
 
             if (userEditText == "" || passEditText == "") {
-                Toast.makeText(this, "لطفا تمامی مقادیر را پر کنید", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.login_toast_message), Toast.LENGTH_SHORT)
+                    .show()
             } else {
-                val intent = Intent(this, MainActivity::class.java)
-                intent.putExtra("message",  "Hello $userEditText")
-                startActivity(intent)
+                val appOpen = preferences.getBoolean("openApp", false)
+
+                if (appOpen) {
+                    if (userEditText == preferences.getString("userNameSave", "test") &&
+                        passEditText == preferences.getString("passSave", "test")
+                    ) {
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.putExtra("message", "Hello $userEditText")
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "نام کاربری یا رمز عبور به درستی وارد نشده است",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                } else {
+                    editor.putString("userNameSave", userEditText).apply()
+                    editor.putString("passSave", passEditText).apply()
+                    editor.putBoolean("openApp", true).apply()
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("message", "Hello $userEditText")
+                    startActivity(intent)
+                }
             }
         }
     }
